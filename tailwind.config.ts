1fb1d2cfd8bef450f3dca0c4264294ead84dd64a
thawancarvalho <thawan.carvalho@aluno.edu.pi.gov.br>
2026-06-12 18:20:27 -0300

    feat: add Tailwind config

---
+++ tailwind.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#3b82f6',
+        secondary: '#1e40af',
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
